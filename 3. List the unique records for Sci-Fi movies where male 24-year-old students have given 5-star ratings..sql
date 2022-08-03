ff85fdfeac1aead80604d5169a45cca760f3d77a
Jade Foster <106618322+jadewfoster@users.noreply.github.com>
2022-08-03 14:39:14 +0100

    Updating 2, 3, 4 MOVIELENS

--- 3. List the unique records for Sci-Fi movies where male 24-year-old students have given 5-star ratings..sql
+++ 3. List the unique records for Sci-Fi movies where male 24-year-old students have given 5-star ratings..sql
@@ -1,7 +1,7 @@
 -- List the unique records for Sci-Fi movies where male 24-year-old students have given 5-star ratings.
 
 SELECT DISTINCT * FROM movielens.genres g
-    INNER JOIN movielens.genres_movies gm ON gm.genre_id = g.id
+	INNER JOIN movielens.genres_movies gm ON gm.genre_id = g.id
 	INNER JOIN movielens.movies m ON m.id = gm.movie_id
 	INNER JOIN movielens.ratings r ON r.movie_id = gm.movie_id
 	INNER JOIN movielens.users u ON u.id = r.user_id
